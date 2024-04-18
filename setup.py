0e0f6a5b2f2b394fe2c237be5dc89f20c708e1ce
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2024-04-18 17:10:23 +0900

    versin up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="blastengine",
-    version="0.0.8",
+    version="0.0.9",
     author="goofmint",
     author_email="atsushi@moongift.jp",
     description="blastengine is SDK for blastengine",
